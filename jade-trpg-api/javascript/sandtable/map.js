cefb4b7f024de992edd80d3a5547b44522516b41
Jade Shan <evokeralucard@gmail.com>
2025-08-23 15:37:36 +0800

    Update map.js

--- jade-trpg-api/javascript/sandtable/map.js
+++ jade-trpg-api/javascript/sandtable/map.js
@@ -100,51 +100,55 @@ exports.handler = {
 		let contenttype = '';
 		let body = null;
 
-		let request = https.get(src, {
-				// add agent for skip 'certificate has expired' error 
-				agent: new https.Agent({ rejectUnauthorized: false, keepAlive: true })}, 
-			(res) => {
-				res.on('data', (chunk) => { buffers.push(chunk); });
-				res.on('close', () => {
-					contenttype = res.headers['content-type'];
-					try {
-						body = Buffer.concat(buffers);
-					} catch (e) {
-						body = '\n    ' + e;
-					}
-					if (res.statusCode !== 200) {
-						console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
-						context.response.writeHead(500, {
-							'Content-Type': contenttype,
-							'Cache-Control': 'public,s-maxage=300,max-age=300',
-							'Access-Control-Allow-Origin': '*',
-							'Access-Control-Allow-Methods': 'GET,POST',
-							'Access-Control-Allow-Headers': 'x-requested-with,content-type'
-						});
-						let errBody = 'Err on get : ' + src;
-						errBody = errBody + '\n   call src err-code: ' + res.statusCode;
+		let getImgResp = new Promise((resolve, reject) => {
+			https.get(imgUrl, {
+				// add agent for skip 'certificate has expired' error
+				agent: new https.Agent({ rejectUnauthorized: false, keepAlive: true })},
+				(res) => {
+					res.on('data', (chunk) => { buffers.push(chunk); });
+					res.on('close', () => {
+						contenttype = res.headers['content-type'];
 						try {
-							errBody = errBody + '\n    ' + body;
+							body = Buffer.concat(buffers);
 						} catch (e) {
-							errBody = errBody + '\n    ' + e;
+							body = '\n    ' + e;
 						}
-						context.response.end(errBody);
-						return;
-					} else {
-						// console.log('Retrieved all data');
-						// 'Cache-Control': 'public,s-maxage=36000,max-age=36000',
-						context.response.writeHead(200, {
-							'Content-Type': contenttype,
-							'Cache-Control': 'private,maxage=36000',
-							'Access-Control-Allow-Origin': '*',
-							'Access-Control-Allow-Methods': 'GET,POST',
-							'Access-Control-Allow-Headers': 'x-requested-with,content-type'
-						});
-						context.response.end(body);
-						return;
-					}
+						if (res.statusCode !== 200) {
+							console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+							context.response.writeHead(500, {
+								'Content-Type': contenttype,
+								'Cache-Control': 'public,s-maxage=300,max-age=300',
+								'Access-Control-Allow-Origin': '*',
+								'Access-Control-Allow-Methods': 'GET,POST',
+								'Access-Control-Allow-Headers': 'x-requested-with,content-type'
+							});
+							let errBody = 'Err on get : ' + imgUrl;
+							errBody = errBody + '\n   call src err-code: ' + res.statusCode;
+							try {
+								errBody = errBody + '\n    ' + body;
+							} catch (e) {
+								errBody = errBody + '\n    ' + e;
+							}
+							context.response.end(errBody);
+							return;
+						} else {
+							// console.log('Retrieved all data');
+							// 'Cache-Control': 'public,s-maxage=36000,max-age=36000',
+							context.response.writeHead(200, {
+								'Content-Type': contenttype,
+								'Cache-Control': 'private,maxage=36000',
+								'Access-Control-Allow-Origin': '*',
+								'Access-Control-Allow-Methods': 'GET,POST',
+								'Access-Control-Allow-Headers': 'x-requested-with,content-type'
+							});
+							context.response.end(body);
+							return;
+						}
+					});
 				});
-			});
+		});
+		//
+		await getImgResp.then(()=>{});
 	},
 
 	// localhost:8088/api/sandtable/map-owner?campaignId=campaign01 
